9dc42c8998f925e7e40dc55aa191fae5ae36c5b1
Bob Tate <btate712@gmail.com>
2020-05-01 16:39:07 -0500

    added two more functions with tests

--- functions.js
+++ functions.js
@@ -2,6 +2,10 @@
 // of multiplying all of those parameters with each other. If no parameters are 
 // passed, it should return the number 1.
 function multiMultiply(...args) {
+  if(args.some(value => typeof(value) !== "number")) {
+    return false;
+  }
+
   if(args.length > 0) {
     return args.reduce((accumulator, nextValue) => accumulator * nextValue, 1);
   } else {
@@ -9,4 +13,28 @@ function multiMultiply(...args) {
   }
 }
 
+// findAny accepts an array and one or more additional parameters and returns true 
+// if any of the additional parameters are found in the array or false if none of the 
+// parameters are found in the array. It should return true if no parameters are passed.
+function findAny(array, ...args) {
+  if(args.length < 1) {
+    return true;
+  } else {
+    return array.some(value => args.some(arg => arg === value));
+  }
+}
+
+// findAll accepts an array and one or more additional parameters and returns true 
+// if all of the additional parameters are found in the array or false if any of the 
+// parameters are not found in the array. It should return true if no parameters are passed.
+function findAll(array, ...args) {
+  if(args.length < 1) {
+    return true;
+  } else {
+    return args.every(arg => array.some(value => value === arg));
+  }
+}
+
 exports.multiMultiply = multiMultiply;
+exports.findAny = findAny;
+exports.findAll = findAll;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,59 @@
 const functions = require("./functions");
 
-test("multiMultiply() should multiply multiple arguments together", () => {
+test("multiMultiply() should multiply multiple parameters together", () => {
   expect(functions.multiMultiply(2,3,4,5)).toBe(120);
+  expect(functions.multiMultiply(5,10,20)).toBe(1000);
+});
+
+test("multiMultiply() should return the passed parameter if only one parameter is passed", () => {
+  expect(functions.multiMultiply(5)).toBe(5);
+})
+
+test("multiMultiply() should return false if any non-numeric parameter is passed", () => {
+  expect(functions.multiMultiply(1,2,3,"four")).toBeFalsy();
+  expect(functions.multiMultiply(1,2,3,"four")).toBe(false);
+});
+
+test("multiMultiply() should return the number 1 if no parameters are passed", () => {
+  expect(functions.multiMultiply()).toBe(1);
+});
+
+test("findAny() should return true if no search parameter is passed", () => {
+  expect(functions.findAny([1,2,3,4,5])).toBe(true);
+});
+
+test("findAny() should return true for a single search parameter if that parameter is in the passed array", () => {
+  expect(functions.findAny([1,2,3,4,5], 4)).toBe(true);
+});
+
+test("findAny() should return false for a single search parameter if that parameter is in the passed array", () => {
+  expect(functions.findAny([1,2,3,4,5], 9)).toBe(false);
+});
+
+test("findAny() should return true for multiple search parameters if any of those parameters are found", () => {
+  expect(functions.findAny([1,2,3,4,5], 7,8,9,3)).toBe(true);
+});
+
+test("findAny() should return false for multiple search parameters if none of those parameters are found", () => {
+  expect(functions.findAny([1,2,3,4,5], 7,8,9)).toBe(false);
+});
+
+test("findAll() should return true if no search parameter is passed", () => {
+  expect(functions.findAll([1,2,3,4,5])).toBe(true);
+});
+
+test("findAll() should return true for a single search parameter if that parameter is in the passed array", () => {
+  expect(functions.findAll([1,2,3,4,5], 4)).toBe(true);
+});
+
+test("findAll() should return false for a single search parameter if that parameter is in the passed array", () => {
+  expect(functions.findAll([1,2,3,4,5], 9)).toBe(false);
+});
+
+test("findAll() should return true for multiple search parameters if all of those parameters are found", () => {
+  expect(functions.findAll([1,2,3,4,5], 2,4,1)).toBe(true);
+});
+
+test("findAll() should return false for multiple search parameters if any of those parameters are not found", () => {
+  expect(functions.findAll([1,2,3,4,5], 1,2,9)).toBe(false);
 });
